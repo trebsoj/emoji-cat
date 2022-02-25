33cbbdd13a7d4413cf45f7b59bbd1034b32bf6d2
Josbert Pedrol <josbert.pedrol@bassolsenergia.com>
2022-02-25 19:17:25 +0100

    script

---
+++ README.md
@@ -0,0 +1,15 @@
+# emoji-cat-selector
+
+Script que et permet tenir un selector d'emojis en català !
+
+![image](https://user-images.githubusercontent.com/75248426/155761589-5473d108-9248-4885-8e0a-5026e279727a.png)
+
+Dependències:
+- Rofi (https://github.com/davatorium/rofi)
+- xcopy
+
+Recomanacions:
+- Configurar una combinació de tecles que executi l'script.
+
+Funcionament:
+- Un cop seleccionat l'emoji que volem, l'script l'escriurà on hi havia abans el focus i ens el guardarà en el porta-retalls.
---
+++ emoji-cat.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+file="/tmp/emoji-cat.list"
+
+# Descarreguem llista d'emojis si no la tenim en el sistema
+if [ ! -f "$file" ]; then
+  curl -sSL 'https://gist.githubusercontent.com/trebsoj/afbccf5bbe1f98a16712f14ad3c5d50e/raw/emoji-cat' >| "$file"
+fi
+
+# Seleccionar emoji
+emoji=$(cat $file | rofi -dmenu -show-icons | awk {'print $1'})
+sleep 0.2
+
+# Escriure emoji on hi havia el focus
+xdotool type $emoji
+
+# Copiem emoji en el porta-retalls
+echo $emoji | xclip -selection clipboard
